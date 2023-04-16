3ec3a7a2f074f814a0d85dbcb976d08ffb1b0d7c
chs98412 <chs98412@naver.com>
2023-04-16 17:03:01 +0900

    project setting

---
+++ src/main/java/com/notodo/notodo/NotodoApplication.java
@@ -0,0 +1,13 @@
+package com.notodo.notodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notodo/notodo/NotodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notodo.notodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
